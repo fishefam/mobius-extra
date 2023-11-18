901ad5f171c30e65dc099872ecf03d36afa06c75
Manh Truong Nguyen <manh-truong.nguyen@gmail.com>
2023-11-18 12:59:26 -0500

    add eslint for auto remove unused imports

--- src/scripts/editor-overlay.ts
+++ src/scripts/editor-overlay.ts
@@ -1,8 +1,3 @@
-import EditorJS from '@editorjs/editorjs';
-import { createElement, getFormData } from '@lib/dom';
-import Header from '@editorjs/header';
-import okatool from '@editorjs/list';
-import { MobiusEditorFormDataKeys } from '@lib/types';
 import { getMobiusEditorData } from '@lib/mobius/editor';
 
 const a = getMobiusEditorData('algorithm');
