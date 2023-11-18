41ac23ace0a2b43bc32cce142c5caef852b2b2de
Manh Truong Nguyen <manh-truong.nguyen@gmail.com>
2023-11-18 13:45:08 -0500

    document and add new utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,49 @@
 import { customAlphabet } from 'nanoid';
 
-export const makePath = (path: string) => browser.runtime.getURL(path);
-export const nanoid = () => customAlphabet('qwertyuiopasdfghjklzxcvbnm', 5)();
+/**
+ * Constructs a fully qualified URL for an internal resource in the extension.
+ *
+ * @param {string} path - The specific path to be converted into a fully qualified URL.
+ * @returns {string} The fully qualified URL.
+ */
+export const makePath = (path: string): string => browser.runtime.getURL(path);
+
+/**
+ * Generates a random alphanumeric string of length 5.
+ *
+ * @returns {string} A random string of length 5.
+ */
+export const nanoid = (): string => customAlphabet('qwertyuiopasdfghjklzxcvbnm', 5)();
+
+/**
+ * Extracts text content outside of script tags from the provided HTML string.
+ *
+ * @param {string} text - The HTML string to be processed.
+ * @returns {string} Text content outside of script tags.
+ */
+export const extractHtmlString = (text: string): string =>
+  text
+    .match(/(.|\n)*<script>|<\/script>(.|\n)*/gim)
+    ?.map((m) => m.replace(/<\/?script>/g, ''))
+    .join('\n') ?? '';
+
+/**
+ * Extracts content within script tags from the provided HTML string.
+ *
+ * @param {string} text - The HTML string to be processed.
+ * @returns {string} Script content from the HTML string.
+ */
+export const extractScriptString = (text: string): string =>
+  text.match(/<script>(.|\n)*<\/script>/gim)?.join('\n') ?? '';
+
+/**
+ * Sets the provided variables as properties on a global `WebExtension` object.
+ *
+ * @param {...{ key: string | number; value: unknown }} vars - Variables to be set globally.
+ */
+export const makeVarsGlobal = (...vars: { key: string | number; value: unknown }[]) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).WebExtension = {};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  vars.forEach(({ key, value }) => ((window as any).WebExtension[`${key}`] = value));
+};
