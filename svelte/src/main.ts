1dfd267a90c166c9b8c7bcfa020b321cb0abc09b
Manh Truong Nguyen <manh-truong.nguyen@gmail.com>
2023-11-19 21:22:36 -0500

    add tailwind

--- svelte/src/main.ts
+++ svelte/src/main.ts
@@ -1,7 +1,6 @@
 import './app.css';
 import App from './App.svelte';
 
-console.log('hi');
 const app = new App({
   target: document.getElementById('svelte-app') as HTMLElement,
 });
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./svelte/src/**/*.{svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
