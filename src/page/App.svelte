748feff5896aac5d97f493d349b4c21158a1db05
Manh Truong Nguyen <manh-truong.nguyen@gmail.com>
2023-11-21 13:59:30 -0500

    restructured

--- src/page/App.svelte
+++ src/page/App.svelte
@@ -4,5 +4,5 @@
 </script>
 
 <main class="w-screen h-screen grid place-items-center text-red-400 font-bold">
-  Hello <span class="first-letter:text-9xl">World</span>
+  Hello <span class="first-letter:text-3xl text-blue-900">World</span>
 </main>
